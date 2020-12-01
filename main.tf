67340b81a80d5319441858f8f83f29df5fe80c72
blakelead <adel.abdelhak.lille@gmail.com>
2020-12-01 11:49:59 +0100

    add: parameters file

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-# Workaround for errors on module deployment: https://github.com/terraform-providers/terraform-provider-azurerm/issues/8840
+# Workaround for errors on module deployment:
+# https://docs.microsoft.com/en-us/azure/templates/microsoft.automation/automationaccounts/modules
+# https://github.com/terraform-providers/terraform-provider-azurerm/issues/8840
 resource "azurerm_resource_group_template_deployment" "this" {
   for_each = { for i, module in var.modules : i => module }
 
@@ -6,13 +8,21 @@ resource "azurerm_resource_group_template_deployment" "this" {
   resource_group_name = var.resource_group_name
   deployment_mode     = "Incremental"
 
-  parameters = {
-    module_name = each.value.name
-    automation_account = var.automation_account_name
-    uri = each.value.uri
-  }
-
   template_content = file("${path.module}/module-template.json")
+
+  parameters_content = templatefile("${path.module}/module-template.parameters.json",
+    {
+      module_name        = each.value.name
+      automation_account = var.automation_account_name
+      uri                = each.value.uri
+    }
+  )
+
+  lifecycle {
+    ignore_changes = [
+      name
+    ]
+  }
 
   tags = var.tags
 }
